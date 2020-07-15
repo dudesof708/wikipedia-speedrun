3fac78b9034303d7574fa0719c58e3cb6d89aa8c
Gideon Tong <gideon@gideontong.com>
2020-07-15 16:02:03 -0700

    Add initial game code

--- main.py
+++ main.py
@@ -1,31 +1,46 @@
 #!/bin/python3
 
+# I'll make this act more like a program later, quick programming
+
 import os
 import wikipedia
 
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def game():
+    players = ""
+    playerList = []
+    clear()
+    print("Game Menu\n---------")
+    while not players.isnumeric():
+        players = input("Number of players > ")
+    players = int(players)
+    for player in range(players):
+        playerList[player] = input("Player " + player + "'s Name? ")
+    playerScores = [0] * players
+    clear()
+
 def generateEndpoints():
-    ep = [wikipedia.random(), wikipedia.random()]
-    print("You are going from", ep[0], "to", ep[1])
+    return [wikipedia.random(), wikipedia.random()]
 
 def pathfind():
     print("Feature coming soon.")
 
 def menu():
-    os.system('cls' if os.name == 'nt' else 'clear')
-    print("""Main Menu
-    ---------
-    1 > Generate Random Endpoints
-    2 > Pathfind
-    3 > Quit
-    """)
+    clear()
+    print("Main Menu\n---------\n1 > Start Game\n2 > Generate Random Endpoints\n3 > Pathfind\n4 > Quit")
     incom = input()
     if incom.isnumeric():
         intcom = int(incom)
         if intcom == 1:
-            generateEndpoints()
+            game()
         elif intcom == 2:
-            pathfind()
+            g = generateEndpoints()
+            print("You are going from", g[0], "to", g[1])
         elif intcom == 3:
+            pathfind()
+        elif intcom == 4:
             exit(0)
     input("Press enter to continue.")
     menu()
