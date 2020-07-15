bc4f5241a2ffddf306a4ce9f9500f136087b3123
Gideon Tong <gideon@gideontong.com>
2020-07-15 16:12:51 -0700

    Implement game rounds

--- main.py
+++ main.py
@@ -8,21 +8,39 @@ import wikipedia
 def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
 
+def stats(cRound, playerList, playerScores, playerTimes):
+    clear()
+    print("nothing to do")
+
+def gameRound(cRound, playerList, playerScores, playerTimes):
+    clear()
+    print("Current round:", cRound)
+    begin, end = generateEndpoints()
+    print("BEGIN AT:", begin, "\n  END AT:", end, '\n')
+    return playerScores, playerTimes
+
 def game():
     players = ""
     playerList = []
+    cRound = 0
     clear()
     print("Game Menu\n---------")
     while not players.isnumeric():
         players = input("Number of players > ")
     players = int(players)
     for player in range(players):
-        playerList[player] = input("Player " + player + "'s Name? ")
+        playerList.append(input("Player " + str(player + 1) + "'s Name? "))
     playerScores = [0] * players
-    clear()
+    playerTimes = [0] * players
+    e = ""
+    while e != "e":
+        cRound += 1
+        playerScores, playerTimes = gameRound(cRound, playerList, playerScores, playerTimes)
+        e = input("Press enter to continue or e to end the game > ")
+    stats(cRound, playerList, playerScores, playerTimes)
 
 def generateEndpoints():
-    return [wikipedia.random(), wikipedia.random()]
+    return wikipedia.random(), wikipedia.random()
 
 def pathfind():
     print("Feature coming soon.")
@@ -36,8 +54,8 @@ def menu():
         if intcom == 1:
             game()
         elif intcom == 2:
-            g = generateEndpoints()
-            print("You are going from", g[0], "to", g[1])
+            begin, end = generateEndpoints()
+            print("You are going from", begin, "to", end)
         elif intcom == 3:
             pathfind()
         elif intcom == 4:
